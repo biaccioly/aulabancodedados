f8a07cb2e36ca0c5eb1c665ef2bbd014b687213d
Aluno <Aluno@Digital.net>
2018-08-31 18:27:01 -0300

    avaliacao bd

--- Livro-52059/Livro.sql
+++ Livro-52059/Livro.sql
@@ -1,4 +1,4 @@
-create table Livro (codigo int primary key, titulo varchar(40), auto varchar (30), genero varchar(10));
+create table Livro (codigo int primary key, titulo varchar(40), autor varchar (30), genero varchar(30));
 
 insert into livro values 
 (1, 'Encontro com Rama', 'Arthur C. Clark', 'Suspense'),
@@ -13,4 +13,14 @@ select * from livro;
 select titulo, genero from livro;
 select * from livro where genero='Suspense';
 select * from livro where autor='Neil Gaiman';
-select * from livro order by titulo desc;
+select * from livro order by titulo;
+select * from livro order by autor desc;
+select * from livro where titulo like 'E%';
+select * from livro where autor like '%n';
+select * from livro where genero like '_i%';
+select * from livro where titulo like '%i_';
+update livro set genero='Suspense' where codigo=6;
+select * from livro;
+delete from livro where codigo = 3;
+select * from livro;
+drop table livro; 
